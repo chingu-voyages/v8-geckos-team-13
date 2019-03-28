b5e03cccb4ce529c3e0d329b7a6c97b4636472b8
James G <jamessgrant@hotmail.co.uk>
2019-03-28 19:10:35 +0000

    Chore: Add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 //this folders purpose is only to import and render the App component
 import React from 'react';
 import {render} from 'react-dom';//i use destructuring whenever i can- if it confuses you just use the syntax u are comfortable with
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 
 
 render(<App />, document.getElementById('root'));
-
-
