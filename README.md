2c49a674d460fb9c0d78ccab3e5fc955bb238824
djordjijem <djordjijem@gmail.com>
2019-03-28 12:58:17 +0100

    update Readme file

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+This project uses create-react-app
+
+Clone the repo and run --> npm install --> npm run start
+
 # v8-geckos-team-13
 Add-project-description-here | Voyage-8 | https://chingu.io/
 
--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+//u use 'default' keyword only when ur exporting ONE thing, and u don't need to name it, because 'default' allows you to name ur modules freely at place of ur import
+//for example; i can say: import MyCoolModule from './App.js', regardles of the name in the App.js (when there is one) file xD (hope this is good explanation xD)
+//practice: in index.js file try to change import App from './App'; to ' import whateverYouWant from ./App.js' 
+export default class extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          App Component
       </div>
     );
   }
 }
 
-export default App;
+
