c62297ef814873a2eeafcebdddf5d1c25e8f0630
James G <jamessgrant@hotmail.co.uk>
2019-03-28 19:12:38 +0000

    Ft: Add separate search icon, change logo image

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -3,7 +3,8 @@
 //if there is a need (and there will be) for sub-component, then, if there is no state involved (if ur sub-component does not contain state)-make everything that u can a sub-component,ie try to modularize ur components as much as posibble - make it a function and place it in the same folder as ur main component, otherwise make a new folder for ur entire component in Components folder and place everything related to ur component there (component, sub-component/s and css files), and then change ur routes in all the places where u import/use it
 import React, {Component} from 'react';
 import './TopBar.css';
-import logo from './logo-geckos-13-team.png';
+import logo from './logo-geckos-13-team.svg';
+import search from './icon-search.svg'
 
 export default class extends Component {
     render(){
@@ -14,11 +15,14 @@ export default class extends Component {
         const menu = ['FEATURED', 'TECHNOLOGY', 'NEWS', 'ENTERTAINMENT', 'DESIGN', 'HEALTH']
         return(
             <div className = "top-bar">
-              <img src = {logo} alt = "logo" />
+              <div class = "top-logo">
+                <img src={logo} alt = "logo" />
+              </div>
               <ul>
                 {menuList(menu)}
               </ul>
               <form>
+              <img src={search} alt="search icon"/>
                 <input type = "text" />
               </form>
              </div>
