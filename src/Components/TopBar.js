1ebd8c6ca7465aa23df7c6065cc7b3a5a8f0547a
James G <jamessgrant@hotmail.co.uk>
2019-03-28 14:34:01 +0000

    Ft: Add main top bar html components

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -2,13 +2,25 @@
 //We use 'class' only if there is 'state' involved, otherwise there will be just normal 'function' exported..for now i will made everything a class cus i(we) don't know what the functionality will look like
 //if there is a need (and there will be) for sub-component, then, if there is no state involved (if ur sub-component does not contain state)-make everything that u can a sub-component,ie try to modularize ur components as much as posibble - make it a function and place it in the same folder as ur main component, otherwise make a new folder for ur entire component in Components folder and place everything related to ur component there (component, sub-component/s and css files), and then change ur routes in all the places where u import/use it
 import React, {Component} from 'react';
+import logo from './logo-geckos-13-team.png'
 
 export default class extends Component {
     render(){
+        const menuList = (list) => list.map((item, index) => {
+          const keyId = `menu-${index}`;
+          return <li key = {keyId}>{item}</li>;
+        });
+        const menu = ['Featured', 'Technology', 'News', 'Entertainment', 'Design', 'Health']
         return(
             <div>
-                 TopBar
+              <img src = {logo} alt = "logo" />
+              <ul>
+                {menuList(menu)}
+              </ul>
+              <form>
+                <input type = "text" />
+              </form>
              </div>
-        )  
+        )
     }
 }
