18cc51a4d9771db57b25b3480dffbd177e1b0385
James G <jamessgrant@hotmail.co.uk>
2019-03-28 19:49:10 +0000

    Ch: Modularise components

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -2,30 +2,22 @@
 //We use 'class' only if there is 'state' involved, otherwise there will be just normal 'function' exported..for now i will made everything a class cus i(we) don't know what the functionality will look like
 //if there is a need (and there will be) for sub-component, then, if there is no state involved (if ur sub-component does not contain state)-make everything that u can a sub-component,ie try to modularize ur components as much as posibble - make it a function and place it in the same folder as ur main component, otherwise make a new folder for ur entire component in Components folder and place everything related to ur component there (component, sub-component/s and css files), and then change ur routes in all the places where u import/use it
 import React, {Component} from 'react';
+import TopBarMenu from './TopBarMenu';
+import TopBarSearch from './TopBarSearch';
 import './TopBar.css';
 import logo from './logo-geckos-13-team.svg';
-import search from './icon-search.svg'
 
 export default class extends Component {
     render(){
-        const menuList = (list) => list.map((item, index) => {
-          const keyId = `menu-${index}`;
-          return <li key = {keyId}>{item}</li>;
-        });
-        const menu = ['FEATURED', 'TECHNOLOGY', 'NEWS', 'ENTERTAINMENT', 'DESIGN', 'HEALTH']
+      const menu = ['FEATURED', 'TECHNOLOGY', 'NEWS', 'ENTERTAINMENT', 'DESIGN', 'HEALTH'];
         return(
             <div className = "top-bar">
-              <div class = "top-logo">
+              <div className = "top-logo">
                 <img src={logo} alt = "logo" />
               </div>
-              <ul>
-                {menuList(menu)}
-              </ul>
-              <form>
-              <img src={search} alt="search icon"/>
-                <input type = "text" />
-              </form>
-             </div>
+              <TopBarMenu menu = {menu} />
+              <TopBarSearch />
+            </div>
         )
     }
 }
