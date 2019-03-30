31d70b031f6dba2df024cd31519504a06910922b
James G <jamessgrant@hotmail.co.uk>
2019-03-30 20:02:32 +0000

    Ft: Move search icon to right of input

--- src/Components/TopBar/TopBarSearch.js
+++ src/Components/TopBar/TopBarSearch.js
@@ -5,10 +5,10 @@ export default class extends Component {
     render(){
         return(
           <form>
+            <input type = "text" />
             <button type ="button" className = "btn btn-link">
               <img src={search} alt="search icon"/>
             </button>
-            <input type = "text" />
           </form>
         )
     }
