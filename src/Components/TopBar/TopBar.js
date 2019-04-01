7cb6036ddd0e20d13b12c99cce5b681d13fd5cda
James G <jamessgrant@hotmail.co.uk>
2019-04-01 23:31:33 +0100

    Style: Centre menu along with collapsed version

--- src/Components/TopBar/TopBar.js
+++ src/Components/TopBar/TopBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 import Search from './TopBarSearch';
 import Menu from './TopBarMenu';
 import logo from './logo-geckos-13-team.svg'
@@ -8,11 +8,8 @@ import './TopBar.css';
 export default class extends Component {
     render(){
       const menu = ['FEATURED', 'TECHNOLOGY', 'NEWS', 'ENTERTAINMENT', 'DESIGN', 'HEALTH'];
-      const MenuList = (list) => list.map( (x, i) =>
-          <Nav.Link key = {i} href={x}>{x}</Nav.Link>
-      );
         return(
-          <Navbar className = "d-flex" expand = "lg" bg="light" fixed="sticky">
+          <Navbar className = "d-flex" expand = "md" bg="light" fixed="sticky">
             <Navbar.Brand className = "logo-width" href="#home">
               <img
                 src={logo}
