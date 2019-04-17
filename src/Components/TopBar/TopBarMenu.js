037d335dda51cfdaa270c83b9ad2bc91fe88fca2
James G <jamessgrant@hotmail.co.uk>
2019-04-17 16:33:42 +0100

    Ch: Change Link to NavLink

--- src/Components/TopBar/TopBarMenu.js
+++ src/Components/TopBar/TopBarMenu.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Nav, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default class extends Component {
     render(){
       const MenuList = (list) => list.map( (x) =>
-          <Link to = {x}>{x.toUpperCase()}</Link>
+          <NavLink to = {`/${x}`}>
+          {x.toUpperCase()}
+          </NavLink>
       );
         return(
             <Nav className = "mr-auto ml-auto flex-wrap menu-bar">
