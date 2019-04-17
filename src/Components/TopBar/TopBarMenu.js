cd20d7d5d60287a26e177c5b1598dd2742ec80fd
James G <jamessgrant@hotmail.co.uk>
2019-04-17 16:10:23 +0100

    Ch: Change to react-route links

--- src/Components/TopBar/TopBarMenu.js
+++ src/Components/TopBar/TopBarMenu.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Nav, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default class extends Component {
     render(){
       const MenuList = (list) => list.map( (x) =>
-          <a href = {x}>{x.toUpperCase()}</a>
+          <Link to = {x}>{x.toUpperCase()}</Link>
       );
         return(
             <Nav className = "mr-auto ml-auto flex-wrap menu-bar">
