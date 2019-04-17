a5390871082a4c3f933da3875bf4d5cb70060015
James G <jamessgrant@hotmail.co.uk>
2019-04-17 17:18:00 +0100

    Ft: Add headlines news api function, add sources

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,21 @@
 import NewsAPI from 'newsapi';
 const newsapi = new NewsAPI('caa53685d9e948c391382626e7bce93e');
 
-export default (input) => {
+const news = (input) => { return newsapi.v2.everything({
+   q: input,
+   sources: 'bbc-news,the-verge,the-new-york-times,techradar,reuters,bloomberg,associated-press,techcrunch,wired',
+   language: 'en',
+   sortBy: 'relevancy',
+   page: 2
+  })
+};
 
- return newsapi.v2.everything({
-         q: input,
-         sources: 'bbc-news,the-verge',
-         domains: 'bbc.co.uk, techcrunch.com',
-         language: 'en',
-         sortBy: 'relevancy',
-         page: 2
-       })
+const headlines = () => { return newsapi.v2.topHeadlines({
+  language: 'en',
+  })
+};
+
+export {
+  news,
+  headlines
 }
