823de616ca660d072e50de876091ded30405620a
James G <jamessgrant@hotmail.co.uk>
2019-04-17 15:42:54 +0100

    Ft: Add routes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@ import {render} from 'react-dom';//i use destructuring whenever i can- if it con
 import 'bootstrap/dist/css/bootstrap.css';
 import './fonts.css';
 import './index.css';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 
-render(<App />, document.getElementById('root'));
+const menu = ['featured', 'technology', 'news', 'entertainment', 'design', 'health'];
+const options = menu.map(item => `/${item}`).map(route => {
+  return <Route
+    path = {route}
+    render={(props) => <App {...props} category = {route} menu = {menu} />}
+  />
+})
+const routing = (
+  <Router>
+    <div>
+      <Route exact path = "/" render={(props) => <App {...props} category = "technology" menu = {menu}/>} />
+      {options}
+    </div>
+  </Router>
+)
+
+render(routing, document.getElementById('root'));
