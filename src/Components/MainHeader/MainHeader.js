2a126c8b814b5c5f1faf4c39d88758753d976ddb
James G <jamessgrant@hotmail.co.uk>
2019-04-18 12:05:30 +0100

    Ft: Change main title to category name

--- src/Components/MainHeader/MainHeader.js
+++ src/Components/MainHeader/MainHeader.js
@@ -6,9 +6,10 @@ import React from 'react';
 import './MainHeader.css';
 
 export default function MainHeader(props) {
+    const title = props.category[0].toUpperCase() + props.category.slice(1);
     return (
         <div className="main-header container  text-center">
-            <h1 className="main-header__title">Featured</h1>
+            <h1 className="main-header__title">{title}</h1>
             <p className="main-header__text">A selection of what you follow</p>
         </div>
     )
