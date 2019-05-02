e0e610f29f79706e1674c9d22d325e76b383146d
James G <jamessgrant@hotmail.co.uk>
2019-05-02 12:48:45 +0100

    Ft: Fetch 100 articles

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,22 +2,21 @@ import NewsAPI from 'newsapi';
 import { key } from '../config.js';
 const newsapi = new NewsAPI(key);
 
-const sources = 'bbc-news,the-verge,the-new-york-times,techradar,reuters,bloomberg,associated-press,techcrunch,wired,fortune,the-economist,the-washington-post,the-wall-street-journal,time,associated-press';
+const sources = 'bbc-news,the-verge,the-new-york-times,techradar,reuters,associated-press,techcrunch,wired,fortune,the-economist,the-washington-post,the-wall-street-journal,time,associated-press';
 
 const news = (input) => { return newsapi.v2.everything({
    q: input,
    sources,
    language: 'en',
    sortBy: 'relevancy',
-   page: 2,
-   pageSize: 50
+   pageSize: 100
   })
 };
 
 const headlines = () => { return newsapi.v2.topHeadlines({
   sources,
   language: 'en',
-  pageSize: 50
+  pageSize: 100
   })
 };
 
