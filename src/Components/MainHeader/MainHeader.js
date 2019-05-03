0e692a49fc56eed92c3e8e9e48c317ff19688d77
James G <jamessgrant@hotmail.co.uk>
2019-05-03 17:52:23 +0100

    Ch: Remove comments

--- src/Components/MainHeader/MainHeader.js
+++ src/Components/MainHeader/MainHeader.js
@@ -1,7 +1,3 @@
-//Jennifer
-//We use 'class' only if there is 'state' involved, otherwise there will be just normal 'function' exported..for now i will made everything a class cus i(we) don't know what the functionality will look like
-//if there is a need (and there will be) for sub-component, then, if there is no state involved (if ur sub-component does not contain state)-make everything that u can a sub-component,ie try to modularize ur components as much as posibble - make it a function and place it in the same file as ur main component, otherwise make a new folder for ur entire component in Components folder and place everything related to ur component there (component, sub-component/s and css files), and then change ur routes in all the places where u import/use it
-
 import React from 'react';
 import './MainHeader.css';
 
